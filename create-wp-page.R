68045e7ce740f222da66690c81665a7dd19101b6
simit patel <simitp@gmail.com>
2020-07-14 12:05:10 -0400

    update

--- create-wp-page.R
+++ create-wp-page.R
@@ -6,12 +6,18 @@
 #'
 #' @return NULL
 #'
-#' @examples create_wp_post()
+#'@examples
+#'\dontrun{
+#'create_wp_page(root_url = 'https://domain.com',user = Sys.getenv('username'),pass = Sys.getenv('password'),
+#'title_val = 'post title',excerpt_val = 'post excerpt',
+#'content_val = 'the post content as a string, with wordpress-accepted <strong>html</strong> (or bbcode!)',
+#'status_val = 'draft',format_val = 'standard')
+#'}
 #'
 #' @export create_wp_post
 
 create_wp_page <- function(root_url,user,pass,title_val,excerpt_val ='',content_val,status_val,author_val,
-                           format_val = 'standard',categories_val, tag_val = '') {
+                           format_val = 'standard') {
   ch = POST(paste0(root_url,"/wp-json/wp/v2/page"),
             authenticate(user,pass),
             body = list(title = title_val,
@@ -19,9 +25,7 @@ create_wp_page <- function(root_url,user,pass,title_val,excerpt_val ='',content_
                         content = content_val,
                         status = status_val,
                         author=author_val,
-                        format=format_val,
-                        categories=categories_val,
-                        tags = tag_val),
+                        format=format_val),
             encode = "json")
   return(ch)
 }
